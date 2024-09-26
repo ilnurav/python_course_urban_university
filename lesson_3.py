4363e22ebee2f5e48eca7eb1adec4ca91dd83673
ilnurav <47894875+ilnurav@users.noreply.github.com>
2024-09-26 22:42:36 +0500

    Add files via upload

---
+++ lesson_3.py
@@ -0,0 +1,8 @@
+# Практическое задание по уроку "Строки и индексация строк"
+
+example = 'HelloWorld!'
+print(example[0])
+print(example[-1])
+print(example[len(example) // 2:])
+print(example[::-1])
+print(example[1::2])
