33de51b9b17aec23afd99818c2b39e4ccdbcc8df
ilnurav <47894875+ilnurav@users.noreply.github.com>
2024-09-26 21:54:15 +0500

    Add files via upload

---
+++ lesson_2.py
@@ -0,0 +1,7 @@
+# Практическое задание по теме "Переменные"
+
+count_home_work = 12
+hours_spent = 1.5
+name_course = 'Python'
+time_per_task = hours_spent / count_home_work
+print(f'Курс: {name_course}, всего задач: {count_home_work}, затрачено часов: {hours_spent}, среднее время выполнения: {time_per_task} часа.')
