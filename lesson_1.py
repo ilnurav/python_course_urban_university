0ec06a90536928a1f86ffad8653d5e87b1ded1f3
ilnurav <47894875+ilnurav@users.noreply.github.com>
2024-09-26 21:33:56 +0500

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,10 @@
+# Практическая работа по уроку "Динамическая типизация"
+
+name = 'Ilnur'
+print('Name:', name)
+age = 37
+print('Age:', age)
+age = age + 1
+print('New Age:', age)
+is_student = True
+print('Is Student:', is_student)
