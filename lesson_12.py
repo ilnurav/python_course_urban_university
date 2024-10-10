7b160f708e19357ea8bef142aded1c14ab77bd7e
Ilnur Akhmetov <47894875+ilnurav@users.noreply.github.com>
2024-10-11 00:21:07 +0500

    Add files via upload

---
+++ lesson_12.py
@@ -0,0 +1,11 @@
+# Дополнительное практическое задание по модулю 2 "Основные операторы"
+
+def passw(n):
+    result = ''
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            if n % (i + j) == 0:
+                result += str(i) + str(j)
+    return result
+
+print(passw(int(input('Введите n: '))))
