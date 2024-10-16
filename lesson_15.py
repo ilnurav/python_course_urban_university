ba33b4e331a5cd2cb4f748a3a9929107c727c249
Ilnur Akhmetov <47894875+ilnurav@users.noreply.github.com>
2024-10-16 20:35:41 +0500

    Update lesson_15.py

--- lesson_15.py
+++ lesson_15.py
@@ -8,18 +8,18 @@ def print_params(a=1, b='строка', c=True):
 print_params(10, 'hello', False) # 10 hello False
 print_params(10, 'hello') # 10 hello True
 print_params(10) # 10 строка True
-print_params(a=10, b='hello', c=False) #
-print_params(a=10, b='hello') # 10 hello False
-print_params(c=10) # 10 hello True
-print_params(b=25) # 1 строка 10
-print_params(c=[1,2,3]) # 1 25 True
+print_params(a=10, b='hello', c=False) # 10 hello False
+print_params(a=10, b='hello') # 10 hello True
+print_params(c=10) # 1 строка 10
+print_params(b=25) # 1 25 True
+print_params(c=[1,2,3]) # 1 строка [1, 2, 3]
 
 # 2. Распаковка параметров
 
-values_list = [3, 'hello', True] # 3 hello True
-values_dict = {'a': 5, 'b': 'world', 'c': True}
-print_params(*values_list) # 3 hello True
-print_params(**values_dict) # 5 world True
+values_list = [3, 'hello', False]
+values_dict = {'a': 5, 'b': 'world', 'c': False}
+print_params(*values_list) # 3 hello False
+print_params(**values_dict) # 5 world False
 
 # 3. Распаковка + отдельные параметры:
 
