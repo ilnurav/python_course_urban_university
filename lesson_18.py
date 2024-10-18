51e93791d39b94064a71b5898ca78e2c0eddc855
Ilnur Akhmetov <47894875+ilnurav@users.noreply.github.com>
2024-10-18 20:44:54 +0500

    Add files via upload

---
+++ lesson_18.py
@@ -0,0 +1,33 @@
+# Дополнительное практическое задание по модулю "Подробнее о функциях"
+
+def calculate_structure_sum(data):
+    total_sum = 0
+    if isinstance(data, dict):
+        for key, value in data.items():
+            total_sum += calculate_structure_sum(value)
+            total_sum += calculate_structure_sum(key)
+    elif isinstance(data, list):
+        for item in data:
+            total_sum += calculate_structure_sum(item)
+    elif isinstance(data, tuple):
+        for item in data:
+            total_sum += calculate_structure_sum(item)
+    elif isinstance(data, set):
+        for item in data:
+            total_sum += calculate_structure_sum(item)
+    elif isinstance(data, (int, float)):
+        total_sum += data
+    elif isinstance(data, str):
+        total_sum += len(data)
+    return total_sum
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result) # 99
