01627e98cb82f5d3abe0eee9b4878c6faf5cdab9
Ilnur Akhmetov <47894875+ilnurav@users.noreply.github.com>
2024-10-28 19:42:16 +0500

    Update lesson_20.py

--- lesson_20.py
+++ lesson_20.py
@@ -3,7 +3,10 @@
 def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
-    inner_function()
+    try:
+        inner_function()
+    except NameError as e:
+        print(e)
 
 test_function() # Я в области видимости функции test_function
 inner_function() # NameError: name 'inner_function' is not defined
