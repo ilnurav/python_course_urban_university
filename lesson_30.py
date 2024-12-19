0c1cd7711e4a2a0a36be6575660049ebb4cc24ad
Ilnur Akhmetov <47894875+ilnurav@users.noreply.github.com>
2024-12-20 00:10:56 +0500

    Update lesson_30.py

--- lesson_30.py
+++ lesson_30.py
@@ -1,2 +1,45 @@
 # Домашнее задание по теме "Режимы открытия файлов"
 
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        try:
+            with open(self.__file_name, 'r', encoding='utf-8') as file:
+                return file.read()
+        except FileNotFoundError:
+            return ""
+
+    def add(self, *products):
+        existing_products = self.get_products().splitlines()
+        with open(self.__file_name, 'a', encoding='utf-8') as file:
+            for product in products:
+                product_str = str(product)
+                if product_str in existing_products:
+                    print(f"Продукт {product.name} уже есть в магазине")
+                else:
+                    file.write(product_str + '\n')
+
+
+s1 = Shop()
+
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
