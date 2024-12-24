911761a4b272459f0058209812409299eea4947c
Ilnur Akhmetov <47894875+ilnurav@users.noreply.github.com>
2024-12-24 19:13:47 +0500

    Add files via upload

---
+++ lesson_36.py
@@ -0,0 +1,42 @@
+# Домашнее задание по теме "Сложные моменты и исключения в стеке вызовов функции"
+
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for item in numbers:
+        try:
+            if not isinstance(item, (int, float)):
+                raise TypeError
+            result += float(item)
+        except (TypeError, ValueError):
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы - {item}')
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    try:
+        if isinstance(numbers, (int, float)):
+            raise TypeError
+        if not isinstance(numbers, (list, tuple, set, str)):
+            raise TypeError
+        if isinstance(numbers, str):
+            numbers = list(numbers)
+        total_sum, incorrect_data = personal_sum(numbers)
+        if len(numbers) == 0:
+            raise ZeroDivisionError
+        numeric_count = len(numbers) - incorrect_data
+        if numeric_count == 0:
+            return 0.0
+        return total_sum / numeric_count
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+        return None
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
