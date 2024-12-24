50fcd86230a63b1e935ae0b32800f8f1f3c2b77c
Ilnur Akhmetov <47894875+ilnurav@users.noreply.github.com>
2024-12-24 12:09:24 +0500

    Update lesson_35.py

--- lesson_35.py
+++ lesson_35.py
@@ -2,13 +2,11 @@
 
 def add_everything_up(a, b):
     try:
-        # Попытка сложить a и b
         return a + b
     except TypeError:
-        # Если возникла ошибка TypeError, возвращаем строковое представление a и b
         return f"{a}{b}"
 
 # Примеры использования
-print(add_everything_up(123.456, 'строка'))  # Вывод: 123.456строка
-print(add_everything_up('яблоко', 4215))    # Вывод: яблоко4215
-print(add_everything_up(123.456, 7))        # Вывод: 130.456
+print(add_everything_up(123.456, 'строка'))  # 123.456строка
+print(add_everything_up('яблоко', 4215))     # яблоко4215
+print(add_everything_up(123.456, 7))         # 130.456
