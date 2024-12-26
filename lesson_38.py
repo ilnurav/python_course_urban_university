414359a60a1dacea764586793615821d244475b3
Ilnur Akhmetov <47894875+ilnurav@users.noreply.github.com>
2024-12-26 22:20:43 +0500

    Create lesson_38.py

---
+++ lesson_38.py
@@ -0,0 +1,13 @@
+Домашнее задание по теме "Введение в функциональное программирование"
+
+def apply_all_func(int_list, *functions):
+    results = {}
+    for func in functions:
+        try:
+            results[func.__name__] = func(int_list)
+        except Exception as e:
+            results[func.__name__] = f"Error: {e}"
+    return results
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
